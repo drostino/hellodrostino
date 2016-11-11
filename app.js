807861a6e36bfa947a783168f516918b0448b633
drostino <psmdrost@gmail.com>
2016-11-11 14:18:33 +0000

    derde versie

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var http = require('http');
 
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
 app.get('/', function(req, res){
-    res.send('hello drostino')
-})
+    res.send('hello drostino');
+});
 
-app.listen(3000, function(){
-    console.log('express server running on port 3000')
-})
+http.createServer(app).listen(app.get('port'),function(){
+    console.log('express server listening on port ' + app.get('port'));
+});
